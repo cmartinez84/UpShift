f975eea4e0fc2286efef59a2c4f62c5800c4c260
chris martinez <chris martinez>
2017-02-03 12:13:33 -0800

    install many map modules and location, add patch to geolocation_googlemaps submodule, add maps to stores/% views

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -245,15 +245,15 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'UpShift',
       'username' => 'admin',
       'password' => 'admin',
-      'host' => 'localhost',
-      'port' => '',
+      'host' => '127.0.0.1',
+      'port' => '8889',
       'driver' => 'mysql',
       'prefix' => '',
     ),
